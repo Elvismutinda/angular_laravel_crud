94eddd5c5584b713cfff0e08c118ab120f23847d
ElvisMutinda <elvismutinda2@gmail.com>
2025-07-21 09:03:02 +0300

    updated UI using devextreme

--- frontend/src/app/student/student.component.ts
+++ frontend/src/app/student/student.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../services/student.service';
+import { DxDataGridModule } from 'devextreme-angular';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DxDataGridModule],
   templateUrl: './student.component.html',
   styleUrl: './student.component.css',
 })
@@ -24,33 +25,61 @@ export class StudentComponent implements OnInit {
     });
   }
 
-  createStudent() {
-    const newStudent = {
-      name: 'Elvis Mutinda',
-      email: 'elvismutinda2@gmail.com',
-      phone: '0112463368',
-    };
+  // createStudent() {
+  //   const newStudent = {
+  //     name: 'Elvis Mutinda',
+  //     email: 'elvismutinda2@gmail.com',
+  //     phone: '0112463368',
+  //   };
 
-    this.studentService.createStudent(newStudent).subscribe(() => {
-      this.loadStudents();
-    });
-  }
+  //   this.studentService.createStudent(newStudent).subscribe(() => {
+  //     this.loadStudents();
+  //   });
+  // }
 
-  updateStudent(id: number) {
-    const updateStudent = {
-      name: 'Dominic Kimeu',
-      email: 'dom@gmail.com',
-      phone: '0796003311',
-    };
+  // updateStudent(id: number) {
+  //   const updateStudent = {
+  //     name: 'Dominic Kimeu',
+  //     email: 'dom@gmail.com',
+  //     phone: '0796003311',
+  //   };
 
-    this.studentService.updateStudent(id, updateStudent).subscribe(() => {
-      this.loadStudents();
-    });
-  }
+  //   this.studentService.updateStudent(id, updateStudent).subscribe(() => {
+  //     this.loadStudents();
+  //   });
+  // }
 
-  deleteStudent(id: number) {
-    this.studentService.deleteStudent(id).subscribe(() => {
-      this.loadStudents();
-    });
-  }
+  // deleteStudent(id: number) {
+  //   this.studentService.deleteStudent(id).subscribe(() => {
+  //     this.loadStudents();
+  //   });
+  // }
+
+  // onEditStudent = (e: any) => {
+  //   const studentId = e.row.data.id;
+  //   this.updateStudent(studentId);
+  // };
+
+  // onDeleteStudent = (e: any) => {
+  //   const studentId = e.row.data.id;
+  //   this.deleteStudent(studentId);
+  // };
+
+  onRowInserted(e: any) {
+  this.studentService.createStudent(e.data).subscribe(() => {
+    this.loadStudents();
+  });
+}
+
+onRowUpdated(e: any) {
+  this.studentService.updateStudent(e.data.id, e.data).subscribe(() => {
+    this.loadStudents();
+  });
+}
+
+onRowRemoved(e: any) {
+  this.studentService.deleteStudent(e.data.id).subscribe(() => {
+    this.loadStudents();
+  });
+}
 }
